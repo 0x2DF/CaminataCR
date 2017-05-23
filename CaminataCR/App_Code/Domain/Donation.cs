a7454b0f2143aa38b079a710ad489fb4661c54ad
0x2DF <p.ares@hotmail.com>
2017-05-23 17:26:45 -0600

    Donations class

---
+++ CaminataCR/App_Code/Domain/Donation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+public class Donation
+{
+    private float amount;
+    private DateTime dateTime;
+    public Donation()
+    {
+    }
+
+    public float Amount
+    {
+        get
+        {
+            return amount;
+        }
+
+        set
+        {
+            amount = value;
+        }
+    }
+
+    public DateTime DateTime
+    {
+        get
+        {
+            return dateTime;
+        }
+
+        set
+        {
+            dateTime = value;
+        }
+    }
+}
