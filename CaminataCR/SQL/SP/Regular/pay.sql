6978ed4fe881d3e781743d04905022ed412e45d2
Wikkan <josuji-alfa@hotmail.com>
2017-06-09 20:38:03 -0500

    Pay

--- CaminataCR/SQL/SP/Regular/pay.sql
+++ CaminataCR/SQL/SP/Regular/pay.sql
@@ -3,15 +3,19 @@ AS
 BEGIN
 
 	DECLARE @totalDonation INT
-
 	SELECT @totalDonation = SUM(D.monto)
 	FROM Donacion D
 	WHERE CONVERT(DATE, D.fechaHora) = CONVERT(DATE, GETDATE())
 
 	SET @totalDonation *= 0.1
 
+	DECLARE @totalLikes INT
+	SELECT @totalLikes = COUNT(*)
+	FROM Likes L
+	WHERE CONVERT(DATE, L.fechaHora) = CONVERT(DATE, GETDATE())
+
 	INSERT INTO CierreDiario(idUsuario, monto, fechaHora)
-		SELECT R.idUsuario, @totalDonation * ((Hiker.Cantidad * 100) / @totalDonation) / 100, CONVERT(DATE, GETDATE())
+		SELECT R.idUsuario, @totalDonation * ((Hiker.Cantidad * 100) / @totalLikes) / 100, CONVERT(DATE, GETDATE())
 		FROM Regular R
 			INNER JOIN (
 				SELECT UC.idUsuario, COUNT(*) Cantidad
